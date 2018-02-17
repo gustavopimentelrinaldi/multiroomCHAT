81858a177b1b3702b47e6b40a5a1c2cc5f91252b
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2018-02-17 21:02:21 -0200

    Websocket connection

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ var server = app.listen(8080, function(){
   console.log("Servidor online!!");
 })
 
-require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+  console.log('Usuário conectou!');
+
+  socket.on('disconnect', function(){
+    console.log('Usuário desconectou!');
+  });
+})
